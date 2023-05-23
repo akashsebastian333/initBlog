d89d25fa9d143779c1c3914be25167c7d4d3979f
AkashSebastian <31592003+akashsebastian333@users.noreply.github.com>
2023-05-23 14:54:29 +0530

    Add files via upload

---
+++ dbChecker.py
@@ -0,0 +1,16 @@
+from helpers import mkdir, exists, message, sqlite3
+
+
+def dbFolder():
+    print("hello")
+
+
+def usersTable():
+    print("hello")
+
+
+def postsTable():
+    print("hello")
+
+def commentsTable():
+    print("hello")
